12238ee6df1ca2d44ba8fd09c0cf8ddb9fef8a15
Matias Ruiz <matiasignacioruiz@gmail.com>
2022-09-01 20:54:08 -0300

    Tipos de Datos

---
+++ tipos_de_datos.py
@@ -0,0 +1,32 @@
+# str
+# str() --> convierte dato a string.
+a="Esto un string"
+
+c = str(120.50)
+print (c, type(c))
+
+print(len(a))
+# seteo de un rango
+print(a[0:4])
+
+print (a[-5])
+
+# Metodos de strings
+print(a.lower())
+print(a.upper())
+# Devuelve un array de elementos conformado por cada palabra.
+print(a.split())
+print(len(a.split()))
+
+# Listas almacena cualquier tipo de datos.
+lista_1=["Hola","5",5]
+
+
+
+
+
+
+
+
+
+
