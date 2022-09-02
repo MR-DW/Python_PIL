772798218b87cd7171cde1f32844c1cf100ab733
Matias Ruiz <matiasignacioruiz@gmail.com>
2022-09-02 17:07:23 -0300

    "continuar pag 72 libro"

---
+++ clase_1/4_variables.py
@@ -0,0 +1,25 @@
+# variable def:  permiten definir nombres para los valores que
+# tenemos en memoria y que vamos a usar en nuestro programa. 
+# Es un espacio guarda información en memoria de la maquina.
+
+# mensaje_usuario = input("ingrese su mensaje")
+# print(mensaje_usuario)
+
+# x=10
+# y= 5
+# z= 2.5
+# variables flotantes (float)
+x= int(input("Ingrese un numero: "))
+y= int(input("Ingrese otro numero: "))
+
+print(type(x))
+print(type(y))
+
+resultado = x+ y
+
+print(type(resultado))
+
+print ("el resultado es: ", resultado)
+
+
+
---
+++ clase_1/objetos.py
@@ -0,0 +1,4 @@
+# todos son objetos y cada uno tiene al menos:
+# un tipo de dato almacenado.
+# un identificador unio para distinguirlo de otros objetos.
+# Un valor consustente con su tipo.
