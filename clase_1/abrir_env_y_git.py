a98f77d19247725ac6247497ad96f37795d96d05
Matias Ruiz <matiasignacioruiz@gmail.com>
2022-09-02 15:16:18 -0300

    "Instructivo de como comenzar y finalizar jornada laboral"

---
+++ clase_1/abrir_env_y_git.py
@@ -0,0 +1,19 @@
+# Para comenzar a trabajar en python, 1ro se abre el cmd.
+# 
+# 2do hay que crear o abrir el entorno virtual, en el cmd, con el siguiente comando:  
+# .\env\Scripts\activate
+# 
+# 3ro Se pullea el último cambio realizado en el repositorio de git hub con:
+# git init
+# git pull
+# Allí ya se descargaron los cambios realizados en la jornada anterior y se puede
+# volver a trabajar.
+# Los commit tambien se pueden realizar directamente desde vsc, en la sección
+# correspondiente.
+# 
+# 4to Realizar git add . y al último el commit para poder ir guardando los cambios
+# realizados.
+
+# 5to desactivar el env con:
+# deactivate
+
